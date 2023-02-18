62b3f66147f2ff72fade173537f4991b48ad24f6
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-02-18 03:41:57 +0100

    Create funny.ads

---
+++ funny.ads
@@ -0,0 +1,14 @@
+{
+  "AdsController": {
+    "adNetwork": "unity",
+    "admobBanner": "ca-app-pub-1010770342437944/1711074865",
+    "admobInterstitial": "ca-app-pub-1010770342437944/2832584845",
+    "applovinBanner": "2ba2a3aeec0e8d92",
+    "applovinInterstitial": "e8307029aae1ec01",
+    "unityGameId": "5147696",
+    "testMode": true,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android",
+    "reviewActive": true
+  }
+}
